87ca4bcd2311667cf42a0a368e44fd7e2a53c006
IsangBaket? <max.bias@myyahoo.com>
2025-03-28 23:46:32 +0800

    fixed code

--- wo_prefix.py
+++ wo_prefix.py
@@ -1,10 +1,14 @@
 #Prog02. removeprefix() remove the characters at the beginning of the string that matches the function parameter.
 #Create a program that do the same functionality without using removeprefix() function.
 
-def max_removeprefix():
-    str = len(str)
-    str[0] == 1
-    print("")
+def max_removeprefix(str, prefix):
+    if len(str) >= len(prefix) and str[:len(prefix)] == prefix:     #searches for the prefix
+        return str[len(prefix):] 
+    return str
 
-str = input("Input any word")
+str = input("Input any word, phrase, or sentence: ")
+prefix = input("Input the prefix that you want to remove: ")
 
+result = max_removeprefix(str, prefix)
+
+print(result)

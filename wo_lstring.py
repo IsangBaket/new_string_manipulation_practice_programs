32c453a9f8402f103d4485e8099e61eaf9977e2f
IsangBaket? <max.bias@myyahoo.com>
2025-03-28 22:47:55 +0800

    removed replace function, instead used while loop

--- wo_lstring.py
+++ wo_lstring.py
@@ -1,5 +1,11 @@
 #Prog01. lstrip() remove the space characters at the beginning of the string. 
 #Create a program that do the same functionality without using lstrip() function.
 
-name = input("Input your name with spaces in the front: ").replace(" ", "")
-print(name)
+name = input("Input your name with spaces in the front: ")
+
+index = 0
+
+while index < len(name) and name[index] == " ":
+    index += 1
+
+print(name[index:])

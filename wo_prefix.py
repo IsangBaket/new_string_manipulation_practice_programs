fce82f7577f694de12198692d28a3ea88c8d4d92
IsangBaket? <max.bias@myyahoo.com>
2025-03-28 23:34:47 +0800

    test

---
+++ wo_prefix.py
@@ -0,0 +1,10 @@
+#Prog02. removeprefix() remove the characters at the beginning of the string that matches the function parameter.
+#Create a program that do the same functionality without using removeprefix() function.
+
+def max_removeprefix():
+    str = len(str)
+    str[0] == 1
+    print("")
+
+str = input("Input any word")
+

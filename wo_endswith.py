27fc3dab88c3765e7f75a30b69f19d850cbbbac9
IsangBaket? <max.bias@myyahoo.com>
2025-04-04 19:47:16 +0800

    pseudocode

---
+++ wo_endswith.py
@@ -0,0 +1,12 @@
+# Prog05. endswith() check if the string end part matches the function parameter. 
+# Create a program that do the same functionality without using endswith() function.
+
+ask user input 
+ask what character ends with 
+
+for characters in user input # iterate over user input
+    loop goes through every string then checks if its same with input 2
+    if user input endswith 
+        print True
+    else 
+        False

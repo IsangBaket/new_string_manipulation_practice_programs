d6ac2b008cafacaa7bfef93ac562a3bca54d3619
IsangBaket? <max.bias@myyahoo.com>
2025-04-04 14:47:22 +0800

    added pseudocode

---
+++ wo_isupper.py
@@ -0,0 +1,14 @@
+# Prog04. isupper() check if all characters of the string is on upper case. 
+# Create a program that do the same functionality without using isupper() function.
+
+
+# this is still a pseudocode I still need to d
+ask user input 
+
+for char in user input # iterate over every character 
+    if all characters in user input is in uppercase
+        print True
+
+    else 
+        print False
+

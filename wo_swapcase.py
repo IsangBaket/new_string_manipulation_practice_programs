cdf712505664450185ad966f8142e905a20bda9e
IsangBaket? <max.bias@myyahoo.com>
2025-04-21 22:35:50 +0800

    added skeleton code

---
+++ wo_swapcase.py
@@ -0,0 +1,11 @@
+# Prog08. swapcase() reverse the casing of each of the character of the string. 
+# Create a program that do the same functionality without using swapcase() function.
+
+def max_swapcase():
+    pass
+
+string = input("Input a string with an upper case and a lower case: ")  # asks user for a string
+
+result = max_swapcase(string)   # calls function
+
+print(result)   # prints result of function

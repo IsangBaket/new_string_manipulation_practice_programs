d4f91cf150ce12c2545c61c2622c7f3eb2830ccd
IsangBaket? <max.bias@myyahoo.com>
2025-04-21 22:42:24 +0800

    problem: only the first character gets transformed

--- wo_swapcase.py
+++ wo_swapcase.py
@@ -1,8 +1,16 @@
 # Prog08. swapcase() reverse the casing of each of the character of the string. 
 # Create a program that do the same functionality without using swapcase() function.
 
-def max_swapcase():
-    pass
+def max_swapcase(string):
+    while True
+        for char in string:
+            result = ''
+            if "A" <= char <= "Z":
+                result += chr(ord(char) + 32)
+            if "a" <= char <= "z":
+                result += chr(ord(char) - 32)
+            return result
+    
 
 string = input("Input a string with an upper case and a lower case: ")  # asks user for a string
 

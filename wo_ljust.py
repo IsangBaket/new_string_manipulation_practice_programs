36b55c9df71ac66516e270728c371e96cd6ab6a4
IsangBaket? <max.bias@myyahoo.com>
2025-04-21 22:02:48 +0800

    added user inputs --- still figuring out function logic

---
+++ wo_ljust.py
@@ -0,0 +1,14 @@
+# Prog06. ljust() add space characters at the end of the string to complete the number of characters specifies in function parameter. 
+# Create a program that do the same functionality without using ljust() function.
+
+def max_ljust(word,padding,string): 
+    pass
+
+
+word = input("Input a word you want to format: ")   # asks user for what word to format
+padding = input("Input how many paddings you want: ")   # asks user how much padding they want
+string = input("Input padding string: ")    # asks what string they want to format the padding with
+
+justified = max_ljust(word,padding,string)  # calls out the function
+
+print(justified)    # prints out the result

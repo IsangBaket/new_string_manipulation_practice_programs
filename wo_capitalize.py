ec27b661a6d1ce9e4a7beb9c311f38b5eb7738c0
IsangBaket? <max.bias@myyahoo.com>
2025-04-21 22:56:06 +0800

    added skeleton code

---
+++ wo_capitalize.py
@@ -0,0 +1,11 @@
+# Prog09. capitalize() makes the first letter of the string, capital letter.
+# And all other letter in small case. Create a program that do the same functionality without using capitalize() function.
+
+def max_capitalize():
+    pass
+
+string = input("Input a word: ")  # asks user for a string
+
+result = max_capitalize(string)   # calls function
+
+print(result)   # prints result of function

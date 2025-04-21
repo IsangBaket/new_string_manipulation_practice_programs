45e7b54aefea50fce890c17e68849d10d7ddd290
IsangBaket? <max.bias@myyahoo.com>
2025-04-21 22:49:15 +0800

    added function body --- solved function logic I literally just copied the code from previous program and just fixed it a little bit to satisfy the logic required

--- wo_swapcase.py
+++ wo_swapcase.py
@@ -2,14 +2,13 @@
 # Create a program that do the same functionality without using swapcase() function.
 
 def max_swapcase(string):
-    while True
-        for char in string:
-            result = ''
-            if "A" <= char <= "Z":
-                result += chr(ord(char) + 32)
-            if "a" <= char <= "z":
-                result += chr(ord(char) - 32)
-            return result
+    result = ''     # this is where the transformed characters are stored
+    for char in string:
+        if "A" <= char <= "Z":      # this transforms every upper case to lower case
+            result += chr(ord(char) + 32)
+        elif "a" <= char <= "z":    # transforms every lower case to upper case
+            result += chr(ord(char) - 32)
+    return result
     
 
 string = input("Input a string with an upper case and a lower case: ")  # asks user for a string
